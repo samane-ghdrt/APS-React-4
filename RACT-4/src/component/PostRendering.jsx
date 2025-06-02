d7def0e545a82d23683bb7a9340359394bbf2c22
samane <samane.ghodrati@gmail.com>
2025-06-02 01:53:12 -0700

    add

--- RACT-4/src/component/PostRendering.jsx
+++ RACT-4/src/component/PostRendering.jsx
@@ -3,17 +3,14 @@ import App from "../App";
 
 
 export default function PostRendering(){
-    const [post,setpost]=useState(0)
+    const [post,setpost]=useState([])
     const API='https://jsonplaceholder.typicode.com/posts';
     useEffect(()=>{
-        fetch(API).then((data)=>{data.json}).then((info)=>{
-            console.log(info);
-            setpost(info)
-        })
+        fetch(API).then((data)=>data.json()).then((info)=>{console.log(info); setpost(info)}).catch(err => console.error("خطا در دریافت داده‌ها:", err));
     },[])
     return(
         <div>
-
+{console.log(post)}
         </div>
     )
 }
--- RACT-4/src/main.jsx
+++ RACT-4/src/main.jsx
@@ -1,8 +1,8 @@
-
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-
+<div>
     <App />
-
+</div>
 )
