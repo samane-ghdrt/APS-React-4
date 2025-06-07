8925af9e7b07d40737436348ad23e711d622d17c
samane <samane.ghodrati@gmail.com>
2025-06-07 02:40:37 -0700

    add

--- RACT-4/src/component/TodoList.jsx
+++ RACT-4/src/component/TodoList.jsx
@@ -2,13 +2,14 @@ import { useState,useEffect } from "react"
 
 
 export default function TodoList(){
-const []=useState([])
+const [data,setData]=useState([])
 
 const todo=async ()=>{
     const res = await fetch(
      "https://jsonplaceholder.typicode.com/todos?_start=0&_limit=5"
    );
    const info =await res.json();
+   setData(info);
    console.log(info)
    
 }
@@ -17,7 +18,21 @@ const todo=async ()=>{
     
     return(
         <div>
-{todo}
+            <div>
+                <input type="text" className="form-control"
+                 />
+                <button  className="btn">
+                    add
+                </button>
+            </div>
+            <div>
+            
+{data.map((i)=>{
+<span>
+    <i></i>
+</span>
+})}
+            </div>
         </div>
     )
 }
