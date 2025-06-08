2dfeed7f141c1cc660fda6f90af5cfa7034b4bac
samane <samane.ghodrati@gmail.com>
2025-06-08 02:38:03 -0700

    add

--- RACT-4/src/component/TodoList.jsx
+++ RACT-4/src/component/TodoList.jsx
@@ -2,36 +2,54 @@ import { useState,useEffect } from "react"
 
 
 export default function TodoList(){
-const [data,setData]=useState([])
-
+const [dataTask,setDataTask]=useState([])
+const [newTask,setNewTask]=useState('');
 const todo=async ()=>{
     const res = await fetch(
      "https://jsonplaceholder.typicode.com/todos?_start=0&_limit=5"
    );
    const info =await res.json();
-   setData(info);
-   console.log(info)
+   setDataTask(info);
    
 }
+const toggleComplete = (id) => {
+    setDataTask(dataTask.map(task =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+    ));
+};
+function handle() {
+ if (newTask.trim() !== "") {
+    setDataTask([...dataTask, { id: dataTask.length+1 , title: newTask, completed: false }]);
+        setNewTask("");
+ }
+}
+const deleteTask = (id) => {
+    setDataTask(dataTask.filter(task => task.id !== id));
+};
 
-     useEffect(()=>{todo},[])
+     useEffect(()=>todo(),[])
+
     
     return(
         <div>
             <div>
-                <input type="text" className="form-control"
+                <input type="text" className="form-control" value={newTask}
                  />
-                <button  className="btn">
+                <button  className="btn"  onClick={handle}>
                     add
                 </button>
             </div>
             <div>
-            
-{data.map((i)=>{
-<span>
-    <i></i>
-</span>
-})}
+             { dataTask.map((item)=>{
+return        <div>
+                  <input type="checkbox" className="form-check-input me-2"
+        checked={item.completed}
+        onChange={() => toggleComplete(item.id)} />  <span style={{ textDecoration: item.completed ? "line-through" : "none" }}>
+        {item.title}
+      </span>
+                  <button className="btn btn-danger btn-sm" onClick={deleteTask(item.id)} ><i className="bi bi-trash"></i> </button>
+              </div>
+             })}
             </div>
         </div>
     )
